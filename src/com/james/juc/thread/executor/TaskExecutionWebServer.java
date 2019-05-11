bea13845950961b88a8efce4fb42b4a913b4d581
Fvamesm <warmhhands@gmail.com>
2019-05-11 16:06:57 +0800

    executor

---
+++ src/com/james/juc/thread/executor/TaskExecutionWebServer.java
@@ -0,0 +1,29 @@
+package com.james.juc.thread.executor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class TaskExecutionWebServer {
+
+    private static final int NTHREADS = 100;
+    private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(8080);
+
+        while (true) {
+            final Socket connection = socket.accept();
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("connection: " + connection);
+                }
+            };
+
+            exec.execute(task);
+        }
+    }
+}
