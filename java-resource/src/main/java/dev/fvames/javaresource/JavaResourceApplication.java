cad1e058a16b2ed2bf771fade62bc0526ce9d7f7
Fvames <warmhhands@gmail.com>
2019-06-04 15:27:13 +0800

    featu：实现 classpath 协议

---
+++ java-resource/src/main/java/dev/fvames/javaresource/JavaResourceApplication.java
@@ -0,0 +1,13 @@
+package dev.fvames.javaresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaResourceApplication.class, args);
+    }
+
+}
